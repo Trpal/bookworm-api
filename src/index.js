1b05c8d1238add67031949a727acaaa8ce80b3e8
Trpal <jonikorpelin@gmail.com>
2018-03-04 21:15:11 +0200

    user account creation

--- src/index.js
+++ src/index.js
@@ -5,14 +5,17 @@ import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import Promise from "bluebird";
 
-import auth from "../routes/auth";
+import auth from "./routes/auth";
+import users from "./routes/users";
 
 dotenv.config();
 const app = express();
 app.use(bodyParser.json());
 mongoose.Promise = Promise;
 mongoose.connect(process.env.MONGODB_URL);
+
 app.use("/api/auth", auth);
+app.use("/api/users", users);
 
 app.get("/*", (req, res) => {
 	res.sendFile(path.join(__dirname, "index.html"));
