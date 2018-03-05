4b0a2f1b05e73fce6c3afe6f173ef12181737614
Trpal <jonikorpelin@gmail.com>
2018-03-05 19:05:15 +0200

    user confirmation email send with mailtrap

--- src/mailer.js
+++ src/mailer.js
@@ -4,11 +4,11 @@ const from = '"Bookworm" <info@bookworm.com>';
 
 function setup() {
 	return nodemailer.createTransport({
-		host: "smtp.mailtrap.io",
-		port: 2525,
+		host: process.env.EMAIL_HOST,
+		port: process.env.EMAIL_PORT,
 		auth: {
-			user: "c3d7f0ea0c6ce8",
-			pass: "a6e5448514e0b9"
+			user: process.env.EMAIL_USER,
+			pass: process.env.EMAIL_PASS
 		}
 	});
 }
@@ -22,7 +22,7 @@ export function sendConfirmationEmail(user) {
 		text: `
 		welcome to Bookworm. please, confirm your email.
 
-		${user.generateConfirmationUrl}
+		${user.generateConfirmationUrl()}
 		`
 	};
 	transport.sendMail(email);
