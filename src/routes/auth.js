71fecb005d60882989220e125155ffa4a7ed9cf2
Trpal <jonikorpelin@gmail.com>
2018-03-09 18:39:21 +0200

    dummy data for bookform

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,42 +1,67 @@
 import express from "express";
 import jwt from "jsonwebtoken";
 import User from "../models/User";
-import { sendResetPasswordEmail } from "../mailer";
+import {
+	sendResetPasswordEmail
+} from "../mailer";
 
 const router = express.Router();
 
 router.post("/", (req, res) => {
-	const { credentials } = req.body;
-	User.findOne({ email: credentials.email }).then(user => {
+	const {
+		credentials
+	} = req.body;
+	User.findOne({
+		email: credentials.email
+	}).then(user => {
 		if (user && user.isValidPassword(credentials.password)) {
-			res.json({ user: user.toAuthJSON() });
+			res.json({
+				user: user.toAuthJSON()
+			});
 		} else {
-			res.status(400).json({ errors: { global: "Invalid credentials" } });
+			res.status(400).json({
+				errors: {
+					global: "Invalid credentials"
+				}
+			});
 		}
 	});
 });
 
 router.post("/confirmation", (req, res) => {
 	const token = req.body.token;
-	User.findOneAndUpdate(
-		{ confirmationToken: token },
-		{ confirmationToken: "", confirmed: true },
-		{ new: true }
-	).then(
+	console.log(req.body);
+	console.log('token', token);
+	User.findOneAndUpdate({
+		confirmationToken: token
+	}, {
+		confirmationToken: "",
+		confirmed: true
+	}, {
+		new: true
+	}).then(
 		user =>
-			user ? res.json({ user: user.toAuthJSON() }) : res.status(400).json({})
+		user ? res.json({
+			user: user.toAuthJSON()
+		}) : res.status(400).json({})
 	);
 });
 
 router.post("/reset_password_request", (req, res) => {
-	User.findOne({ email: req.body.email }).then(user => {
+	User.findOne({
+		email: req.body.email
+	}).then(user => {
 		if (user) {
 			sendResetPasswordEmail(user);
 			res.json({});
 		} else {
 			res
 				.status(400)
-				.json({ errors: { global: "There is no user with such email" } });
+				.json({
+					errors: {
+						global: "There is no user with such email"
+					}
+				});
 		}
 	});
 });
@@ -52,17 +77,30 @@ router.post("/validate_token", (req, res) => {
 });
 
 router.post("/reset_password", (req, res) => {
-	const { password, token } = req.body.data;
+	const {
+		password,
+		token
+	} = req.body.data;
 	jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
 		if (err) {
-			res.status(401).json({ errors: { global: "Invalid token" } });
+			res.status(401).json({
+				errors: {
+					global: "Invalid token"
+				}
+			});
 		} else {
-			User.findOne({ _id: decoded._id }).then(user => {
+			User.findOne({
+				_id: decoded._id
+			}).then(user => {
 				if (user) {
 					user.setPassword(password);
 					user.save().then(() => res.json({}));
 				} else {
-					res.status(404).json({ errors: { global: "Invalid token" } });
+					res.status(404).json({
+						errors: {
+							global: "Invalid token"
+						}
+					});
 				}
 			});
 		}
